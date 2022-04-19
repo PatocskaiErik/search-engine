475ccf9f5ee3bd846b6ad8f2d1008003a254c536
Erik <patoerik93@gmail.com>
2022-04-19 18:09:05 +0200

    Deleted unused items and installed mui

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Homepage</h1>
+      <div className="home-header">Gmail, Images</div>
+      <div className="home-body">Google</div>
+    </div>
+  );
+};
+export default Home;
